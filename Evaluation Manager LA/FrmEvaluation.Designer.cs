eb873a30a571891384fe204f3b3b623f1047985b
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-15 19:14:57 +0200

    Insert/Update evaluacije povezan s formom.

--- Evaluation Manager LA/FrmEvaluation.Designer.cs
+++ Evaluation Manager LA/FrmEvaluation.Designer.cs
@@ -127,6 +127,7 @@
             this.btnSave.TabIndex = 9;
             this.btnSave.Text = "Spremi";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
